d40f5228a7d2bb401a28e0ec6d51e5a6dd586a31
Minjee <grace_123@nate.com>
2024-12-29 23:36:31 +0900

    스프링북마켓 BookRepository(2단계.저장소 객체 생성하기)

---
+++ src/main/java/com/springmvc/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.springmvc.repository;
+
+import java.util.List;
+
+import com.springmvc.domain.Book;
+
+public interface BookRepository {
+	List<Book> getAllBookList(); //List 가 ArrayList 의 부모 클래스
+}
