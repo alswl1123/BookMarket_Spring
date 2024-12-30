9d2b53140d091e8fca8d3ab6c711ae2f34713f1f
Minjee <grace_123@nate.com>
2024-12-30 23:27:53 +0900

    스프링북마켓 BookController.java(컨트롤러에 @RequestMapping 작성하기)

--- src/main/java/com/springmvc/controller/BookController.java
+++ src/main/java/com/springmvc/controller/BookController.java
@@ -3,7 +3,7 @@ package com.springmvc.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller; //필요1
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,13 +11,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.springmvc.domain.Book;
 import com.springmvc.service.BookService;
 
-@Controller
+@Controller //필요2
+@RequestMapping("/books") //추가
 public class BookController {
 
 	@Autowired
 	private BookService bookService;
 	
-	@RequestMapping(value="/books", method=RequestMethod.GET)
+	//@RequestMapping(value="/books", method=RequestMethod.GET) 이거였는데
+	@RequestMapping //이렇게 수정
 	public String requestBookList(Model model) {
 		List<Book> list = bookService.getAllBookList();
 		model.addAttribute("bookList", list);
