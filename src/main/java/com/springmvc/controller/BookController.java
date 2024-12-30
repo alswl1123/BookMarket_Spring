0aadd263f3416f27565c88bb7a4dc5043d5d7651
Minjee <grace_123@nate.com>
2024-12-30 23:33:50 +0900

    스프링북마켓 BookController (단순화한 @RequestMapping 사용하기)

--- src/main/java/com/springmvc/controller/BookController.java
+++ src/main/java/com/springmvc/controller/BookController.java
@@ -3,23 +3,30 @@ package com.springmvc.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller; //필요1
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping; //이걸 추가하면서
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestMethod; //이건 사용x
 
 import com.springmvc.domain.Book;
 import com.springmvc.service.BookService;
 
-@Controller //필요2
-@RequestMapping("/books") //추가
+@Controller
+@RequestMapping("/books")
 public class BookController {
 
 	@Autowired
 	private BookService bookService;
 	
+	@GetMapping //지워버려서 추가함. GetMapping 으로 수정1
+	public String requestBookList(Model model) {
+		List<Book> list = bookService.getAllBookList();
+		model.addAttribute("bookList", list);
+		return "books";
+	}
 
-	@RequestMapping("/all")
+	@GetMapping("/all") //수정2
 	public String requestAllBooks(Model model) {
 		List<Book> list = bookService.getAllBookList();
 		model.addAttribute("bookList", list);
