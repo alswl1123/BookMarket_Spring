d38590d73436dbd82886d27b586b99c086a62a03
Minjee <grace_123@nate.com>
2024-12-31 23:16:39 +0900

    스프링북마켓 BookController (@PathVariable)

--- src/main/java/com/springmvc/controller/BookController.java
+++ src/main/java/com/springmvc/controller/BookController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView; //추가
 
@@ -33,5 +34,12 @@ public class BookController {
 		modelAndView.addObject("bookList", list);
 		modelAndView.setViewName("books");
 		return modelAndView;
+	}
+	
+	@GetMapping("/{category}") //추가
+	public String requestBooksByCategory(@PathVariable("category") String bookCategory, Model model) {
+		List<Book> booksByCategory=bookService.getBookListByCategory(bookCategory);
+		model.addAttribute("bookList", booksByCategory);
+		return "books";
 	}
 }
