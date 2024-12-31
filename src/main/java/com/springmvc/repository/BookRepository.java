b2d67ea278b65bef4dd39f950f29db62237ab25e
Minjee <grace_123@nate.com>
2024-12-31 23:23:30 +0900

    스프링북마켓 BookRepository (@PathVariable)

--- src/main/java/com/springmvc/repository/BookRepository.java
+++ src/main/java/com/springmvc/repository/BookRepository.java
@@ -6,4 +6,5 @@ import com.springmvc.domain.Book;
 
 public interface BookRepository {
 	List<Book> getAllBookList(); //List 가 ArrayList 의 부모 클래스
+	List<Book> getBookListByCategory(String category);
 }
--- src/main/java/com/springmvc/repository/BookRepositoryImpl.java
+++ src/main/java/com/springmvc/repository/BookRepositoryImpl.java
@@ -46,4 +46,14 @@ public class BookRepositoryImpl implements BookRepository {
 		return listOfBooks;
 	}
 	
+	public List<Book> getBookListByCategory(String category) {
+		List<Book> booksByCategory = new ArrayList<Book>();
+		for(int i = 0; i<listOfBooks.size(); i++) {
+			Book book = listOfBooks.get(i);
+			if(category.equalsIgnoreCase(book.getCategory()))
+				booksByCategory.add(book);
+		}
+		return booksByCategory;
+	}
+	
 }
