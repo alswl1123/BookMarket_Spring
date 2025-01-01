51bba082db35d9ae718a146e8f87e746ef64b3af
Minjee <grace_123@nate.com>
2025-01-01 23:25:28 +0900

    스프링북마켓 BookController (@MatrixVariable)

--- src/main/java/com/springmvc/controller/BookController.java
+++ src/main/java/com/springmvc/controller/BookController.java
@@ -1,11 +1,14 @@
 package com.springmvc.controller;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.MatrixVariable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView; //추가
@@ -41,5 +44,12 @@ public class BookController {
 		List<Book> booksByCategory=bookService.getBookListByCategory(bookCategory);
 		model.addAttribute("bookList", booksByCategory);
 		return "books";
+	}
+	
+	@GetMapping("/filter/{bookFilter}")
+	public String requestBooksByFilter(@MatrixVariable(pathVar="bookFilter") Map<String, List<String>> bookFilter, Model model) {
+			Set<Book> booksByFilter = bookService.getBookListByFilter(bookFilter);
+			model.addAttribute("bookList", booksByFilter);
+			return "books";
 	}
 }
