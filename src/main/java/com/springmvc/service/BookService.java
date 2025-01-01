108cb162207dfa012e852a96f8cc0b4589af8c3d
Minjee <grace_123@nate.com>
2025-01-01 23:29:58 +0900

    스프링북마켓 BookService (@MatrixVariable)

--- src/main/java/com/springmvc/service/BookService.java
+++ src/main/java/com/springmvc/service/BookService.java
@@ -1,10 +1,14 @@
 package com.springmvc.service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+// import com.springmvc.controller.Set; //import 이거 아님!
 import com.springmvc.domain.Book;
 
 public interface BookService {
 	List<Book> getAllBookList();
 	List<Book> getBookListByCategory(String bookCategory);
+	Set<Book> getBookListByFilter(Map<String, List<String>> bookFilter);
 }
--- src/main/java/com/springmvc/service/BookServiceImpl.java
+++ src/main/java/com/springmvc/service/BookServiceImpl.java
@@ -1,6 +1,8 @@
 package com.springmvc.service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,6 +23,13 @@ public class BookServiceImpl implements BookService {
 		List<Book> booksByCategory = bookRepository.getBookListByCategory(category);
 		return booksByCategory;
 	}
+
+	@Override //이렇게 Override 해서 작성할 수도 있음
+	public Set<Book> getBookListByFilter(Map<String, List<String>> filter) {
+		Set<Book> booksByFilter = bookRepository.getBookListByFilter(filter);
+		return booksByFilter;
+	}
+	
 	
 
 }
