c7be3da29a934374a54105be630888f1adc32d5e
Minjee <grace_123@nate.com>
2025-01-11 20:26:23 +0900

    스프링북마켓 BookController (이미지 등록)

--- src/main/java/com/springmvc/controller/BookController.java
+++ src/main/java/com/springmvc/controller/BookController.java
@@ -1,14 +1,17 @@
 package com.springmvc.controller;
 
+import java.io.File;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.WebDataBinder; //이 import 필요
-import org.springframework.web.bind.annotation.InitBinder; //이 import 필요 
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.MatrixVariable;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -16,11 +19,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView; //추가
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.springmvc.domain.Book;
-import com.springmvc.repository.BookRepositoryImpl;
 import com.springmvc.service.BookService;
+
 
 @Controller
 @RequestMapping("/books")
@@ -72,14 +76,28 @@ public class BookController {
 	}
 	
 	
-	//매개변수 수준
+	//순서1.파일이름 2.빈 파일 생성 3.파일 기록(transferTo)
 	@PostMapping("/add")
-	public String submitAddNewBook(@ModelAttribute("NewBook") Book book) {
+	public String submitAddNewBook(@ModelAttribute("NewBook") Book book, HttpServletRequest request) {
+		
+		MultipartFile bookImage = book.getBookImage();
+		String save = request.getServletContext().getRealPath("/resources/images");
+		String saveName = bookImage.getOriginalFilename();
+		File saveFile = new File(save, saveName);
+		
+		if (bookImage != null && !bookImage.isEmpty()) {
+			try {
+				bookImage.transferTo(saveFile);
+			} catch (Exception e) {
+				throw new RuntimeException("도서 이미지 업로드가 실패하였습니다", e);
+			}
+		}
+		
 		bookService.setNewBook(book);
-		return "redirect:/books"; //url 에 ? 붙음(파라미터를 가져가서 쓰지는 않는다) // 슬래시(/)를 붙여서 절대경로를 반환해야 함.
-	}
+		return "redirect:/books";
 	
-	//메서드 수준
+		}
+	
 	@ModelAttribute
 	public void addAttributes(Model model) {
 		model.addAttribute("addTitle","신규 도서 등록"); //key, value 구조
@@ -91,6 +109,6 @@ public class BookController {
 	//날짜, 숫자 등의 형식을 일관되게 처리
 	@InitBinder
 	public void initBinder(WebDataBinder binder) {
-		binder.setAllowedFields("bookId", "name", "unitPrice", "author", "description", "publisher", "category", "unitsInStock", "totalPages", "releaseDate", "condition");	
+		binder.setAllowedFields("bookId", "name", "unitPrice", "author", "description", "publisher", "category", "unitsInStock", "totalPages", "releaseDate", "condition", "bookImage");	
 		}
 }
