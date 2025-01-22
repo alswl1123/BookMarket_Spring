a2dafb3ecaad86abb877811a3d212103d59c0b24
Minjee <grace_123@nate.com>
2025-01-22 22:48:45 +0900

    스프링북마켓 BookId(오류 메시지 가져오도록 클래스 수정)

--- src/main/java/com/springmvc/validator/BookId.java
+++ src/main/java/com/springmvc/validator/BookId.java
@@ -7,6 +7,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import javax.validation.Constraint;
+import javax.validation.Payload;
 
 
 @Constraint(validatedBy=BookIdValidator.class)
@@ -14,7 +15,7 @@ import javax.validation.Constraint;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface BookId { //생김새 주의. 만들 때는 클래스로 생성
-	String message() default "";
+	String message() default "{BookId.NewBook.bookId}";
 	Class<?>[] groups() default{};
-	Class<?>[] payload() default{};
+	public abstract Class<? extends Payload>[] payload() default{};
 }
