3fa513c0fc3f6367d664748d250d977b60020ff4
Minjee <grace_123@nate.com>
2025-01-23 14:41:08 +0900

    스프링북마켓 Cart 서비스 인터페이스, 구현체 (RESTful)

---
+++ src/main/java/com/springmvc/service/CartServiceImpl.java
@@ -0,0 +1,21 @@
+package com.springmvc.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.springmvc.domain.Cart;
+import com.springmvc.repository.CartRepository;
+
+public class CartServiceImpl implements CartService {
+	
+	@Autowired
+	private CartRepository cartRepository;
+	
+	public Cart create(Cart cart) {
+		return cartRepository.create(cart);
+	}
+	
+	public Cart read(String cartId) {
+		return cartRepository.read(cartId);
+	}
+
+}
