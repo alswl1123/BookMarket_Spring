4dd6ec9f2226cee91bc4a696bb98063b72cb5b8a
Minjee <grace_123@nate.com>
2025-01-25 23:50:56 +0900

    스프링북마켓 controllers.js (removeFromCart 메서드 추가)

--- src/main/webapp/resources/js/controllers.js
+++ src/main/webapp/resources/js/controllers.js
@@ -3,3 +3,9 @@ function addToCart(action) {
 	document.addForm.submit();
 	alert("도서가 장바구니에 추가되었습니다!");
 }
+
+function removeFromCart(action) {
+	document.removeForm.action = action;
+	document.removeForm.submit();
+	window.location.reload();
+}
